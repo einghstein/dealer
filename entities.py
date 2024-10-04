ad36939d2a42096224a38cf5273033f50a8e22ae
einghstein <103892112+einghstein@users.noreply.github.com>
2024-10-04 17:42:59 +0200

    Update entities.py

--- entities.py
+++ entities.py
@@ -51,42 +51,43 @@ class Char:
     y = 1080//2
     sirka = img.get_width()
     vyska = img.get_height()
-    rychlost = 9
-    wheat = 100
+    rychlost = 8
+    wheat = 1
     coin = 0
+    shoot_cooldown = 0
     def walk(self,vse,window):
             keys = pygame.key.get_pressed()
-            if keys[pygame.K_d] and self.x < window.x-self.sirka:
+            if keys[pygame.K_d] and self.x < 1920-self.sirka:
                 self.x = self.x+self.rychlost
                 self.img = pygame.image.load("images/right.png")
-                #for t in vse:
-                #    if t != self and not(isinstance(t, Cop)):
-                #        if check_coll_class(self,t):
-                #self.x = (self.x-self.rychlost)
+                for t in vse:
+                    if t != self:
+                        if check_coll_class(self,t):
+                            self.x = (self.x-self.rychlost)
 
             if keys[pygame.K_a] and self.x > self.rychlost:
                 self.x = self.x-self.rychlost
                 self.img = pygame.image.load("images/left.png")
-                #for t in vse:
-                #    if t != self and not(isinstance(t, Cop)):
-                #        if check_coll_class(self,t):
-                #self.x = (self.x+self.rychlost)
+                for t in vse:
+                    if t != self:
+                        if check_coll_class(self,t):
+                            self.x = (self.x+self.rychlost)
 
             if keys[pygame.K_w] and self.y > self.rychlost:
                 self.y = self.y-self.rychlost
                 self.img = pygame.image.load("images/back.png")
-                #for t in vse:
-                #    if t != self and not(isinstance(t, Cop)):
-                #        if check_coll_class(self,t):
-                #self.y = self.y+self.rychlost
+                for t in vse:
+                    if t != self:
+                        if check_coll_class(self,t):
+                            self.y = self.y+self.rychlost
 
-            if keys[pygame.K_s] and self.y+self.vyska < window.y:
+            if keys[pygame.K_s] and self.y < 1080-self.vyska:
                 self.y = self.y+self.rychlost
                 self.img = pygame.image.load("images/forward.png")
-                #for t in vse:
-                #    if t != self and not(isinstance(t, Cop)):
-                #        if check_coll_class(self,t):
-                #self.y = self.y-self.rychlost
+                for t in vse:
+                    if t != self:
+                        if check_coll_class(self,t):
+                            self.y = self.y-self.rychlost
 
 class Bullet:
     def __init__(self,x,y,mx,my,shooter):
@@ -106,63 +107,14 @@ class Bullet:
         self.x -= self.x_vel
         self.y -= self.y_vel
         for e in bullets:
-            if e != self.shooter:
-                if check_coll_class(self,e) and not(isinstance(e,Bullet)):
-                    try:
-                        bullets.remove(e)
-                        bullets.remove(self)
-                    except:
-                        pass
-        if self.x < 0 or self.x > window.x:
-            try:
+            if e != self.shooter and e != self:
+                if check_coll_class(self,e):
+                    bullets.remove(e)
+                    bullets.remove(self)
+        try:
+            if self.x < 0 or self.x > window.get_width():
                 bullets.pop(bullets.index(self))
-            except:
-                pass
-        if self.y < 0 or self.y > window.y:
-            try:
+            if self.y < 0 or self.y > window.get_width():
                 bullets.pop(bullets.index(self))
-            except:
-                pass
-
-class Cop:
-    def __init__(self, x, y, is_angry, tox, toy):
-        self.x = x
-        self.y = y
-        self.img = pygame.image.load("images/cop_front.png")
-        self.sirka = self.img.get_width()
-        self.vyska = self.img.get_height()
-        self.agro = is_angry
-        self.dir_x = tox
-        self.dir_y = toy
-        self.speed = 9 if is_angry else 7
-    def walk(self,vse,window):
-        for i in range(1,self.speed):
-            if self.dir_x == self.x:
-                if self.dir_y == self.y:
-                    if self.agro:
-                        self.dir_x = vse[0].x
-                        self.dir_y = vse[0].y
-                    else:
-                        self.dir_x = randint(0,window.x-self.sirka)
-                        self.dir_y = randint(0,window.y-self.vyska)
-            else:
-                if self.x > self.dir_x:
-                    self.x -= 1
-                    self.img = pygame.image.load("images/cop_left.png")
-                else:
-                    self.x += 1
-                    self.img = pygame.image.load("images/cop_right.png")
-            if self.dir_y != self.y:
-                if self.y > self.dir_y:
-                    self.y -= 1
-                    self.img = pygame.image.load("images/cop_back.png")
-                else:
-                    self.y += 1
-                    self.img = pygame.image.load("images/cop_front.png")
-        
-class Car:
-    def __init__(self,x,y,speed,is_cop):
-        self.x = x
-        self.y = y
-        self.speed = speed
-        self.is_cop = is_cop
+        except:
+            print("lag")  
